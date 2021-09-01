f45669008c4b8a1bf6bc4f1b43d4e72efd4ad014
taha <shajariantaha@gmial.com>
2021-09-02 01:57:10 +0430

    bounce object in game

--- src/index.js
+++ src/index.js
@@ -8,22 +8,54 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      // gravity: { y: 200 }
+      debug: true
     }
   },
   scene: {
-    preload: preload,
-    create: create
+    render,
+    preload,
+    create,
+    update,
   }
 };
-
-new Phaser.Game(config);
+const VELOCITY = 250
+const Game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png')
 }
 
 function create() {
   const bgImage = this.add.image(0, 0, 'sky');
   bgImage.setOrigin(0)
+  this.bird = this.physics.add.sprite(Game.config.width * 0.1, Game.config.height / 2, 'bird')
+  // this.bird.body.gravity.x = 100
+  // bird.setOrigin(0)
+
+  this.bird.body.velocity.x = VELOCITY
+  this.bird.body.velocity.y = VELOCITY
+}
+
+function update(time, delta) {
+  // console.log(delta)
+  // this.bird.body.gravity.x -= 3
+  // console.log(this.bird)
+  if (this.bird.x >= config.width - this.bird.width / 2) {
+    this.bird.body.velocity.x = -VELOCITY
+  }
+  if (this.bird.x - this.bird.width / 2 <= 0) {
+    this.bird.body.velocity.x = VELOCITY
+  }
+  if (this.bird.y >= config.height - this.bird.height / 2) {
+    this.bird.body.velocity.y = -VELOCITY
+  }
+  if (this.bird.y - this.bird.height / 2 <= 0) {
+    this.bird.body.velocity.y = VELOCITY
+  }
+}
+
+function render() {
+  console.log('render runned')
 }
