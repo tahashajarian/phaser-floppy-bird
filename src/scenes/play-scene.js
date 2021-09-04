eafa612bd2d14cf1f8462195188b9c9f5301117f
طاها شجریان <t.shajarian@pdnco>
2021-09-04 18:17:06 +0430

    add graphic

--- src/scenes/play-scene.js
+++ src/scenes/play-scene.js
@@ -1,91 +1,87 @@
-import BaseScene from './base-scene';
-import { scenesName, BEST_SCORE } from './constance';
-
-
+import BaseScene from "./base-scene";
+import { scenesName, BEST_SCORE } from "./constance";
 
 class PlayScene extends BaseScene {
   constructor(config) {
-    super(scenesName.PLAY_SCENE, config)
-    this.birdJumpVelocity = -300
+    super(scenesName.PLAY_SCENE, config);
+    this.birdJumpVelocity = -300;
+    this.isPaused = false;
   }
 
   create() {
-    super.create()
+    super.create();
+    this.createMountains();
+    this.createClouds();
+    this.createPipes();
+    this.createGround();
     this.createBird();
     this.handleInputs();
-    this.createPipes();
-    this.createPause()
-    this.handleCollisWorld()
-    this.handleCollisBirdAndPipe()
-    this.createScore()
-    this.listenToEvents()
-    this.createAnims()
+    this.createPause();
+    this.handleCollisWorld();
+    this.handleCollisBirdAndPipe();
+    this.createScore();
+    this.listenToEvents();
+    this.createAnims();
+    this.isPaused = false;
   }
 
-  createAnims() {
-    this.anims.create({
-      key: 'fly',
-      frames: this.anims.generateFrameNumbers('bird', { start: 9, end: 15 }),
-      frameRate: 32,
-      repeat: 1
-    })
-
-    this.anims.create({
-      key: 'die',
-      frames: this.anims.generateFrameNumbers('bird', { start: 16, end: 18 }),
-      frameRate: 8,
-      // repeat: 0
-    })
-
-    this.bird.play('fly')
-
+  createClouds() {
+    this.clouds = this.physics.add.group();
+    for (let i = 0; i < 9; i++) {
+      const cloud = this.clouds.create(0, 0, "cloud");
+      this.placeCloud(cloud);
+    }
+    this.clouds.setVelocity(-30, 0);
   }
-  createBG() {
-    this.add.image(0, 0, 'sky').setOrigin(0);
+
+  createGround() {
+    this.grounds = this.physics.add.group();
+    for (let i = 0; i < 5; i++) {
+      const ground = this.grounds.create(0, 0, "ground").setImmovable(true);
+
+      this.placeGround(ground);
+      ground.setBodySize(ground.width, ground.height - 40);
+    }
+    this.groundSize = 170;
+    this.grounds.setVelocity(-200, 0);
   }
 
   createBird() {
-    this.bird = this.physics.add.sprite(this.config.birdInitialPosition.x, this.config.birdInitialPosition.y, 'bird')
+    this.bird = this.physics.add
+      .sprite(
+        this.config.birdInitialPosition.x,
+        this.config.birdInitialPosition.y,
+        "bird"
+      )
       .setScale(3)
-      .setFlipX(true)
-    this.bird.setBodySize(this.bird.width, this.bird.height - 8)
+      .setFlipX(true);
+    this.bird.setBodySize(this.bird.width, this.bird.height - 8);
     this.bird.setCollideWorldBounds(true);
-    this.bird.body.gravity.y = 600
-    this.bird.body.onWorldBounds = true
-  }
-
-  createPipes() {
-    this.pipes = this.physics.add.group();
-    for (let i = 0; i < 3; i++) {
-      const pipeTop = this.pipes.create(0, 0, 'pipe').setImmovable(true).setOrigin(0, 1)
-      const pipeBottom = this.pipes.create(0, 0, 'pipe').setImmovable(true).setOrigin(0, 0)
-      pipeTop.setDisplaySize(pipeTop.width, this.config.height)
-      pipeBottom.setDisplaySize(pipeBottom.width, this.config.height)
-      this.placePipe(pipeTop, pipeBottom)
-    }
-    this.pipes.setVelocity(-200, 0)
-  }
-
-  createPause() {
-    const pauseButton = this.add.image(this.config.width - 10, this.config.height - 5, 'pause')
-      .setInteractive()
-      .setOrigin(1)
-      .setScale(3)
-
-    pauseButton.on('pointerdown', () => {
-      this.pauseGame()
-    })
+    this.bird.body.gravity.y = 600;
+    this.bird.body.onWorldBounds = true;
   }
 
   handleInputs() {
-    this.input.on('pointerdown', this.flap.bind(this))
-    this.input.keyboard.on('keydown_SPACE', () => this.flap())
-    this.input.keyboard.on('keydown_UP', () => this.flap())
-    this.input.keyboard.on('keydown_ESC', () => this.pauseGame())
+    this.input.on("pointerdown", this.flap.bind(this));
+    this.input.keyboard.on("keydown_SPACE", () => this.flap());
+    this.input.keyboard.on("keydown_UP", () => this.flap());
+    this.input.keyboard.on("keydown_ESC", () => this.pauseGame());
+  }
+
+  createPause() {
+    const pauseButton = this.add
+      .image(this.config.width - 10, this.config.height - 5, "pause")
+      .setInteractive()
+      .setOrigin(1)
+      .setScale(3);
+
+    pauseButton.on("pointerdown", () => {
+      this.pauseGame();
+    });
   }
 
   handleCollisWorld() {
-    this.physics.world.on('worldbounds', (body, up, down, left, right) => {
+    this.physics.world.on("worldbounds", (body, up, down, left, right) => {
       if (down || up) {
         this.gameOver();
       }
@@ -93,138 +89,267 @@ class PlayScene extends BaseScene {
   }
 
   handleCollisBirdAndPipe() {
-    this.physics.add.collider(this.bird, this.pipes, this.gameOver, null, this)
-  }
-
-  listenToEvents() {
-    if (this.eventListener) return
-    this.eventListener = this.events.on('resume', () => {
-      this.resumeCountDownText = this.add.text(...this.centerScreen, 'Continue in 3', this.fontStyle).setOrigin(0.5)
-      this.resumeCountDown = 3
-      this.timeEvent = this.time.addEvent({
-        delay: 500,
-        callback: () => this.countDown(),
-        loop: true
-      })
-    })
-  }
-
-  countDown() {
-    this.resumeCountDown--;
-    this.resumeCountDownText.setText(`Continue in ${this.resumeCountDown}`)
-    if (this.resumeCountDown <= 0) {
-      this.isPaused = false
-      this.timeEvent.remove();
-      this.physics.resume();
-      this.resumeCountDownText.setText('')
-    }
+    this.physics.add.collider(this.bird, this.pipes, this.gameOver, null, this);
+    this.physics.add.collider(
+      this.bird,
+      this.grounds,
+      this.gameOver,
+      null,
+      this
+    );
   }
 
   createScore() {
     this.score = 0;
-    this.scoreText = this.add.text(16, 16, `Score: 0`, { fontSize: '20px', fill: '#222' })
-    this.BestScoreText = this.add.text(16, 40, `Best Score: ${this.bestScore || 0}`, { fontSize: '16px', fill: '#444' })
+    this.scoreText = this.add.text(16, 16, `Score: 0`, {
+      fontSize: "20px",
+      fill: "#222",
+    });
+    this.BestScoreText = this.add.text(
+      16,
+      40,
+      `Best Score: ${this.bestScore || 0}`,
+      { fontSize: "16px", fill: "#444" }
+    );
+  }
+
+  listenToEvents() {
+    if (this.eventListener) return;
+    this.eventListener = this.events.on("resume", () => {
+      this.resumeCountDownText = this.add
+        .text(...this.centerScreen, "Continue in 3", this.fontStyle)
+        .setOrigin(0.5);
+      this.resumeCountDown = 3;
+      this.timeEvent = this.time.addEvent({
+        delay: 500,
+        callback: () => this.countDown(),
+        loop: true,
+      });
+    });
+  }
+
+  createAnims() {
+    this.anims.create({
+      key: "fly",
+      frames: this.anims.generateFrameNumbers("bird", { start: 9, end: 15 }),
+      frameRate: 32,
+      repeat: 1,
+    });
+
+    this.anims.create({
+      key: "die",
+      frames: this.anims.generateFrameNumbers("bird", { start: 16, end: 18 }),
+      frameRate: 8,
+      // repeat: 0
+    });
+
+    this.bird.play("fly");
+  }
+
+  countDown() {
+    this.resumeCountDown--;
+    this.resumeCountDownText.setText(`Continue in ${this.resumeCountDown}`);
+    if (this.resumeCountDown <= 0) {
+      this.isPaused = false;
+      this.timeEvent.remove();
+      this.physics.resume();
+      this.resumeCountDownText.setText("");
+    }
   }
 
   saveBestScore() {
     if (this.score > this.bestScore) {
-      localStorage.setItem(BEST_SCORE, this.score)
+      localStorage.setItem(BEST_SCORE, this.score);
     }
   }
 
   gameOver() {
-    this.isPaused = true
-    console.log('GAME OVER')
-    this.physics.pause();
-    this.bird.setTint(0xee4824)
-    this.bird.play('die')
-    this.bird.y -= 10
-    this.saveBestScore()
-    this.time.addEvent({
+    if (this.isPaused) return;
+    // if (this.isPaused) return;
+    this.isPaused = true;
+    // this.physics.pause();
+    this.pipes.setVelocity(0, 0);
+    this.grounds.setVelocity(0, 0);
+    this.bird.setTint(0xee4824);
+    this.bird.play("die");
+    this.saveBestScore();
+    this.restrartTime = this.time.addEvent({
       delay: 1000,
       callback: () => {
         this.scene.restart();
-        this.isPaused = false
+        this.pipes.setVelocity(-200, 0);
+        this.grounds.setVelocity(-200, 0);
+        this.isPaused = false;
       },
-      loop: false
-    })
+      loop: false,
+      // callbackScope: this,
+    });
   }
 
   pauseGame() {
-    this.isPaused = true
+    this.isPaused = true;
     this.physics.pause();
     this.scene.pause();
-    this.scene.launch(scenesName.PAUSE_SCENE)
+    this.scene.launch(scenesName.PAUSE_SCENE);
   }
 
-
   update() {
-    this.recyclePipe()
+    this.recyclePipe();
   }
 
   recyclePipe() {
     const arrayPipes = this.pipes.getChildren();
-    const passedPipe = []
-    arrayPipes.forEach(pipe => {
+    const passedPipe = [];
+    arrayPipes.forEach((pipe) => {
       if (pipe.getBounds().right < 0) {
-        passedPipe.push(pipe)
+        passedPipe.push(pipe);
         if (passedPipe.length === 2) {
-          this.placePipe(...passedPipe)
-          this.increasScore()
+          this.placePipe(...passedPipe);
+          this.increasScore();
         }
       }
-    })
+    });
+    const arrayClouds = this.clouds.getChildren();
+    arrayClouds.forEach((cloud) => {
+      if (cloud.getBounds().right < 0) {
+        this.placeCloud(cloud);
+      }
+    });
+    const arrayMountains = this.mountains.getChildren();
+    arrayMountains.forEach((mountain) => {
+      if (mountain.getBounds().right < 0) {
+        this.placeMountain(mountain);
+      }
+    });
+    const arrayGrounds = this.grounds.getChildren();
+    arrayGrounds.forEach((ground) => {
+      if (ground.getBounds().right < 0) {
+        this.placeGround(ground);
+      }
+    });
   }
 
   flap() {
-    if (this.isPaused) return
-    // // this if for limit jump speed :/
-    // if (this.bird.body.velocity.y < -200) {
-    //   this.bird.body.velocity.y = -200
-    // } else {
-    //   this.bird.body.velocity.y += this.birdJumpVelocity
-    // }
-    this.bird.play('fly')
-    this.bird.body.velocity.y += this.birdJumpVelocity
-
-  }
-
-  restart() {
-    this.bird.body.velocity.y = 0
-    this.bird.x = this.config.birdInitialPosition.x
-    this.bird.y = this.config.birdInitialPosition.y
+    if (this.isPaused) return;
+    this.bird.play("fly");
+    this.bird.body.velocity.y += this.birdJumpVelocity;
   }
 
   placePipe(uPipe, bPipe) {
-    let pipeHorizontalDistance = this.getRightLastPipe() + 500
+    let pipeHorizontalDistance = this.getRightLastPipe() + 500;
     let pipeVerticalDistance = 200;
-    // difculty Game 
+    // difculty Game
     if (this.score > 1) {
-      pipeVerticalDistance -= this.score * 2
-      pipeHorizontalDistance -= this.score * 5
+      pipeVerticalDistance -= this.score * 2;
+      pipeHorizontalDistance -= this.score * 5;
     }
-    const pipePositionRange = Phaser.Math.Between(20, this.config.height - 20 - pipeVerticalDistance)
-    uPipe.x = pipeHorizontalDistance
-    uPipe.y = pipePositionRange
-    bPipe.x = uPipe.x
-    bPipe.y = uPipe.y + pipeVerticalDistance
+    const pipePositionRange = Phaser.Math.Between(
+      20,
+      this.config.height - this.groundSize - 20 - pipeVerticalDistance
+    );
+    uPipe.x = pipeHorizontalDistance;
+    uPipe.y = pipePositionRange;
+    bPipe.x = uPipe.x;
+    bPipe.y = uPipe.y + pipeVerticalDistance;
+  }
+
+  placeCloud(cloud) {
+    const cloundHorizontalDistance =
+      this.getRightLastCloud() + Phaser.Math.Between(200, 250);
+    const cloundPositionRange = Phaser.Math.Between(20, 200);
+    cloud.x = cloundHorizontalDistance;
+    cloud.y = cloundPositionRange;
+    cloud.setScale(Math.random() * (0.7 - 0.3) + 0.3);
+  }
+
+  placeMountain(mountain) {
+    const mountainHorizontalDistance =
+      this.getRightLastMountain() + Phaser.Math.Between(700, 800);
+    const mountainPositionRange = this.config.height - this.groundSize;
+    mountain.setOrigin(1, 1);
+    mountain.x = mountainHorizontalDistance;
+    mountain.y = mountainPositionRange;
+    mountain.setScale(Math.random() * (1 - 0.9) + 0.9);
+  }
+
+  placeGround(ground) {
+    const GroundHorizontalDistance = this.getRightLastGround() + 870;
+    const GroundPositionRange = this.config.height;
+    ground.setOrigin(1, 1);
+    ground.x = GroundHorizontalDistance;
+    ground.y = GroundPositionRange;
   }
 
   getRightLastPipe() {
-    let rightLastPipe = 0
+    let rightLastPipe = -500;
     const arrayPipes = this.pipes.getChildren();
     arrayPipes.forEach((pipe) => {
-      rightLastPipe = pipe.x > rightLastPipe ? pipe.x : rightLastPipe
+      rightLastPipe = pipe.x > rightLastPipe ? pipe.x : rightLastPipe;
     });
-    return rightLastPipe
+    return rightLastPipe;
+  }
+
+  getRightLastCloud() {
+    let rightLastCloud = 0;
+    const arrayClouds = this.clouds.getChildren();
+    arrayClouds.forEach((cloud) => {
+      rightLastCloud = cloud.x > rightLastCloud ? cloud.x : rightLastCloud;
+    });
+    return rightLastCloud;
+  }
+
+  getRightLastMountain() {
+    let rightLastMountain = 0;
+    const arrayMountains = this.mountains.getChildren();
+    arrayMountains.forEach((mountain) => {
+      rightLastMountain =
+        mountain.x > rightLastMountain ? mountain.x : rightLastMountain;
+    });
+    return rightLastMountain;
+  }
+
+  getRightLastGround() {
+    let rightLastGround = 0;
+    const arrayGrounds = this.grounds.getChildren();
+    arrayGrounds.forEach((ground) => {
+      rightLastGround = ground.x > rightLastGround ? ground.x : rightLastGround;
+    });
+    return rightLastGround;
   }
 
   increasScore() {
     this.score++;
-    this.scoreText.setText(`Score: ${this.score}`)
-    this.saveBestScore()
+    this.scoreText.setText(`Score: ${this.score}`);
+    this.saveBestScore();
   }
 
+  createMountains() {
+    this.mountains = this.physics.add.group();
+    for (let i = 0; i < 5; i++) {
+      const mountain = this.mountains.create(0, 0, "mountain");
+      mountain.setDisplaySize(mountain.width, mountain.height * 2);
+
+      this.placeMountain(mountain);
+    }
+    this.mountains.setVelocity(-20, 0);
+  }
+  createPipes() {
+    this.pipes = this.physics.add.group();
+    for (let i = 0; i < 4; i++) {
+      const pipeTop = this.pipes
+        .create(0, 0, "pipe")
+        .setImmovable(true)
+        .setOrigin(0, 1);
+      const pipeBottom = this.pipes
+        .create(0, 0, "pipe")
+        .setImmovable(true)
+        .setOrigin(0, 0);
+      pipeTop.setDisplaySize(pipeTop.width, this.config.height);
+      pipeBottom.setDisplaySize(pipeBottom.width, this.config.height);
+      this.placePipe(pipeTop, pipeBottom);
+    }
+    this.pipes.setVelocity(-200, 0);
+  }
 }
 
 export default PlayScene;
--- src/scenes/preload-scene.js
+++ src/scenes/preload-scene.js
@@ -1,28 +1,30 @@
-import BaseScene from './base-scene';
-import { scenesName } from './constance';
+import BaseScene from "./base-scene";
+import { scenesName } from "./constance";
 
 class PreloadScene extends BaseScene {
-    constructor(config) {
-        super(scenesName.PRELOAD_SCENE, config)
-    }
+  constructor(config) {
+    super(scenesName.PRELOAD_SCENE, config);
+  }
 
-    preload() {
-        this.load.image('sky', 'assets/sky.png');
-        this.load.spritesheet('bird', 'assets/birdSprite.png', {
-            frameWidth: 16, frameHeight: 16
-        });
-        this.load.image('pipe', 'assets/pipe.png');
-        this.load.image('pause', 'assets/pause.png');
-        this.load.image('back', 'assets/back.png');
-    }
+  preload() {
+    this.load.image("sky", "assets/sky.png");
+    this.load.spritesheet("bird", "assets/birdSprite2.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+    this.load.image("pipe", "assets/pipe.png");
+    this.load.image("pause", "assets/pause.png");
+    this.load.image("back", "assets/back.png");
+    this.load.image("cloud", "assets/cloud.png");
+    this.load.image("mountain", "assets/mountain.png");
+    this.load.image("ground", "assets/ground.png");
+  }
 
-    create() {
-        this.scene.start(scenesName.MENU_SCENE)
-    }
+  create() {
+    this.scene.start(scenesName.MENU_SCENE);
+  }
 
-    update() {
-
-    }
+  update() {}
 }
 
 export default PreloadScene;
